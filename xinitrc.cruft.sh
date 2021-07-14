36137555ef9d1933469d4b21568d60ea03c21a15
fluffywaffles <skorlir@gmail.com>
2021-07-14 00:12:22 -0500

    fixup! xinitrc: refactor, split out crufty provided stuff to xinitrc.cruft.sh

--- xinitrc.cruft.sh
+++ xinitrc.cruft.sh
@@ -9,11 +9,11 @@ usermodmap=${HOME}/.Xmodmap
 sysresources=/etc/X11/xinit/.Xresources
 sysmodmap=/etc/X11/xinit/.Xmodmap
 # Merge system-wide configuration files into X defaults
-if [ -f ${sysresources} ]; then   xrdb -merge ${sysresources}   fi
-if [ -f ${sysmodmap}    ]; then   xmodmap ${sysmodmap}          fi
+if [ -f ${sysresources} ]; then   xrdb -merge ${sysresources};   fi
+if [ -f ${sysmodmap}    ]; then   xmodmap ${sysmodmap};          fi
 # Merge user configuration files into X defaults
-if [ -f ${userresources} ]; then  xrdb -merge ${userresources}  fi
-if [ -f ${usermodmap}    ]; then  xmodmap ${usermodmap}         fi
+if [ -f ${userresources} ]; then  xrdb -merge ${userresources};  fi
+if [ -f ${usermodmap}    ]; then  xmodmap ${usermodmap};         fi
 
 # Run any system-wide X startup shell scripts
 if [ -d /etc/X11/xinit/xinitrc.d ] ; then
@@ -21,4 +21,3 @@ if [ -d /etc/X11/xinit/xinitrc.d ] ; then
     [ -x "${xinitd_script}" ] && . "${xinitd_script}"
   done
 fi
-
