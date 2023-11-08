3c9bcb65f478d642c4bbf866f43ce369180cf43e
fluffywaffles <skorlir@gmail.com>
2023-11-08 11:31:48 -0800

    bugfix: st: st-install.sh should use st, not st-git, and support patches

--- st-install.sh
+++ st-install.sh
@@ -5,11 +5,15 @@
 mkdir -p ~/software
 cd ~/software
 
-yay -G st-git
+yay -G st
 
 cd ~/software/st
 
 ln -s ~/.st-config.h config.h
 ln -s ~/.st-colors colors
+
+for diff in ~/.st-patches/*.diff; do
+  ln -s ${diff}
+done
 
 makepkg --syncdeps --install
